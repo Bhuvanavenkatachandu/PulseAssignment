c80eb1ea65b8903501d15e28b4f375ab6417b4a9
Bhuvana Venkata Chandu <eluribhuvanavenkatachandu@gmail.com>
2025-12-26 12:52:42 +0530

    CorsUpdate

--- backend/server.js
+++ backend/server.js
@@ -16,9 +16,8 @@ const io = new Server(server, {
     }
 });
 
-// Middleware
 app.use(cors({
-    origin: ['http://localhost:5174', 'http://127.0.0.1:5174'],
+    origin: ['http://localhost:5174', 'https://pulseassignment.netlify.app'],
     credentials: true
 }));
 app.use(express.json());
